37a435c1d89ed5b2b5e7dab0630f4af5883de49c
Jbt <hilasheto1@gmail.com>
2024-09-30 11:02:49 +0300

    display #1

--- screen.py
+++ screen.py
@@ -1,6 +1,16 @@
 import pygame
 
 pygame.init()
-# back_ground_color =
-screen = pygame.display.set_mode((700, 600))
+
+screen = pygame.display.set_mode((800, 700))
+
+pygame.display.set_caption('Geeks for geeks')
+
+pygame.display.flip()
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
