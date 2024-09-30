3b06298aecc9f2a65a68594c57f821df1205a5e0
dorin <hananiadorin7@gmail.com>
2024-09-30 10:49:43 +0300

    c

---
+++ game_field.py
@@ -0,0 +1,10 @@
+def create_board():
+    main_list = []
+    for i in range(25):
+        second_list = []
+        for j in range(50):
+            main_list.append(second_list)
+            print(*main_list)
+
+
+def
