c6baff6998936e84eff1907ea771ee7f204ca1ab
Jbt <hilasheto1@gmail.com>
2024-09-30 14:40:51 +0300

    display #3

--- screen.py
+++ screen.py
@@ -1,4 +1,6 @@
 import pygame
+from game_field import create_board
+from game_field import put_mine
 
 pygame.init()
 
@@ -9,8 +11,32 @@ pygame.display.set_caption('The Flag')
 green = (0, 50, 0)
 black = (0, 0, 0,)
 
-screen.fill(green)
-pygame.display.update()
+put_mine()
+
+grid_node_width = 20
+grid_node_height = 20
+
+
+def create_square(z, y, color):
+    pygame.draw.rect(screen, color, [z, y, grid_node_width, grid_node_height])
+
+
+def visualize_grid():
+    y = 0
+    for row in create_board():
+        z = 0
+        for item in row:
+            if item == 0:
+                create_square(z, y, (255, 255, 255))
+            else:
+                create_square(z, y, green)
+
+            z += grid_node_width
+        y += grid_node_height
+    pygame.display.update()
+
+
+visualize_grid()
 
 running = True
 while running:
@@ -23,3 +49,5 @@ while running:
                 pygame.draw.line(screen, green, (1, x), (1000, x), 2)
                 pygame.draw.line(screen, green, (x, 1), (x, 1000), 2)
                 pygame.display.update()
+
+
