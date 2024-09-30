0f69fc0784d34a3d738a45886143ac306fa235db
Jbt <hilasheto1@gmail.com>
2024-09-30 11:38:12 +0300

    display #2

--- screen.py
+++ screen.py
@@ -2,11 +2,11 @@ import pygame
 
 pygame.init()
 
-screen = pygame.display.set_mode((800, 700))
+screen = pygame.display.set_mode((1000, 500))
 
-pygame.display.set_caption('Geeks for geeks')
+pygame.display.set_caption('The Flag')
 
-pygame.display.flip()
+green = (0, 50, 0)
 
 running = True
 while running:
@@ -14,6 +14,10 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    for x in range(0, 1000, 20):
+        pygame.draw.line(screen,green,(1,x), (1000, x),2)
+        pygame.draw.line(screen, green, (x, 1), (x, 1000), 2)
+        pygame.display.update()
 
 
 
