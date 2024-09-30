53b23eaf6abdd7d5b7e8f3472edc53252255d3b6
dorin <hananiadorin7@gmail.com>
2024-09-30 15:54:26 +0300

    day one 15:54

--- game_field.py
+++ game_field.py
@@ -12,10 +12,29 @@ def create_board():
     return board
 
 
-def put_mine():
+def put_man():
     board = create_board()
+    for i in range(0, 5):
+        for j in range(0, 2):
+            if i < 3:
+                board[i][j] = "body"
+            else:
+                board[i][j] = "leg"
+    return board
+
+
+def put_flag():
+    board = put_man()
+    for i in range(21, 25):
+        for j in range(46, 50):
+            board[i][j] = "flag"
+    return board
+
+
+def put_mine():
+    board = put_flag()
     total_count = 0
-    while total_count < 60:
+    while total_count < 20:
         random_row_number = random.randint(0, 24)
         for i in range(25):
             if i == random_row_number:
@@ -36,11 +55,11 @@ def put_mine():
 def put_bush():
     board = put_mine()
     total_count = 0
-    while total_count < 60:
+    while total_count < 20:
         value = True
         while value:
             random_row_number = random.randint(0, 24)
-            if random_row_number > 1:
+            if random_row_number < 24:
                 value = False
         for i in range(25):
             if i == random_row_number:
@@ -49,13 +68,19 @@ def put_bush():
                 while random_col_number > 47:
                     random_col_number = random.randint(1, 49)
                 for k in range(random_col_number, random_col_number + 3):
-                    if board[random_row_number][k] == "FREE" or "mine" \
-                            and board[random_row_number + 1] == "FREE" or "mine":
+                    if board[random_row_number][k] == "FREE" or "mine" and board[
+                        random_row_number + 1] == "FREE" or "mine":
                         count += 1
                         total_count += 1
                 if count == 3:
                     total_count += 1
                     for s in range(random_col_number, random_col_number + 3):
-                        board[random_row_number + 1][s] = "bush"
-                        board[random_row_number][s] = "bush"
+                        if board[random_row_number + 1][s] == "mine":
+                            board[random_row_number + 1][s] = "mine-bush"
+                        else:
+                            board[random_row_number + 1][s] = "bush"
+                        if board[random_row_number][s] == "mine":
+                            board[random_row_number][s] = "mine-bush"
+                        else:
+                            board[random_row_number][s] = "bush"
     return board
---
+++ soldier.py
@@ -0,0 +1,7 @@
+import game_field
+import pygame
+
+
+def soldier_move():
+    board = game_field.put_bush()
+    
