2fea5096a51ddfa4f788d38ed998424bbfe5710e
Jbt <hilasheto1@gmail.com>
2024-10-01 16:46:40 +0300

    printing the screens

--- main.py
+++ main.py
@@ -4,19 +4,23 @@ import pygame
 import screen
 import soldier
 
+soldier_loc = [0, 0]
+cube = 20
 
 running = True
 while running:
     key_pressed = pygame.key.get_pressed()
     for event in pygame.event.get():
+
         if event.type == pygame.QUIT:
             running = False
         screen.print_normal()
-        soldier.move_soldier()
         if key_pressed[pygame.K_RETURN]:
             screen.black_screen()
             time.sleep(1)
             screen.print_normal()
+
+
 
 
 
--- soldier.py
+++ soldier.py
@@ -68,18 +68,18 @@ cube = 20
 def move_soldier_2():
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
-            if event.KEY == pygame.K_UP:
+            if event.key == pygame.K_UP:
                 soldier_loc[0] -= cube
-            elif event.KEY == pygame.K_DOWN:
+            elif event.key == pygame.K_DOWN:
                 soldier_loc[0] -= cube
-            elif event.KEY == pygame.K_LEFT:
+            elif event.key == pygame.K_LEFT:
                 soldier_loc[1] -= cube
-            elif event.KEY == pygame.K_RIGHT:
+            elif event.key == pygame.K_RIGHT:
                 soldier_loc[1] += cube
     img = pygame.image.load('soldier.png')
     img = pygame.transform.scale(img, (60, 60))
     screen.screen.blit(img, (soldier_loc[0] * 20, soldier_loc[1] * 20))
-    pygame.display.update()
+    return pygame.display.update()
 
 
 
